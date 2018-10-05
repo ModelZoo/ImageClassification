4d47b483ec09a9485c377ed3ebdae38062aed58b
Germey <cqc@cuiqingcai.com>
2018-10-06 01:56:20 +0800

    update model

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from model_zoo.trainer import BaseTrainer
 from tensorflow.python.keras.datasets import fashion_mnist
 
-tf.flags.DEFINE_integer('epochs', 20, 'Max epochs')
+tf.flags.DEFINE_integer('epochs', 100, 'Max epochs')
 tf.flags.DEFINE_float('learning_rate', 0.01, 'Learning rate')
 
 
@@ -14,12 +14,12 @@ class Trainer(BaseTrainer):
         self.model_class = FashionMnistModel
     
     def prepare_data(self):
-        (x_train, y_train), (x_eval, y_eval) = fashion_mnist.load_data()
-        x_train, x_eval = x_train.reshape((-1, 28, 28, 1)), x_eval.reshape((-1, 28, 28, 1))
-        x_train, x_eval = x_train.astype('float32') / 255.0, x_eval.astype('float32') / 255.0
-        y_train, y_eval = y_train.astype('float32'), y_eval.astype('float32')
-        y_train = tf.keras.utils.to_categorical(y_train, 10)
-        y_eval = tf.keras.utils.to_categorical(y_eval, 10)
+        (x_train, y_train), (_, _) = fashion_mnist.load_data()
+        x_train = x_train.reshape((-1, 28, 28, 1))
+        x_train, y_train = x_train.astype('float32') / 255.0, \
+                           tf.keras.utils.to_categorical(y_train.astype('float32'), 10)
+        (x_train, x_eval) = x_train[5000:], x_train[:5000]
+        (y_train, y_eval) = y_train[5000:], y_train[:5000]
         train_data, eval_data = (x_train, y_train), (x_eval, y_eval)
         return train_data, eval_data
 
